14e9cd44467c0c9b9c4c603e473c8ea07041a684
Hydrochilli <mainzer10@gmail.com>
2024-04-16 12:12:34 +0100

    04-get-art-byID

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { getAllTopics } = require('./controllers/topics.controller');
 const {getEndpoint} = require('./controllers/api-endpoints.controller')
+const {getArticleByID} = require('./controllers/articles.controller')
 const express = require('express');
 const app = express();
 
@@ -7,11 +8,17 @@ app.use(express.json())
 
 app.get('/api/topics', getAllTopics)
 app.get('/api', getEndpoint)
+app.get('/api/articles/:article_id', getArticleByID)
 
 
 app.use('/*', (req, res, next) => {
-    res.status(404).send({ message: 'page not found' });
+    res.status(404).send({ message: 'route is invalid' });
   });
 
-
+app.use((err, req, res,  next)  => {
+    const statusCode = err.status || 500
+    res.status(statusCode).send({
+        message: err.message
+    })
+})
 module.exports = app
